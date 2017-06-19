abb00bd54b8b77bf8b8e5fbf83e9d66b0a4745d1
ManahariDahal <manahari.dahal1@gmail.com>
2017-06-19 17:35:49 -0400

    Add files via upload

---
+++ EmployeeTester.java
@@ -0,0 +1,63 @@
+// Name: Manahari Dahal     Date Assigned: 25 January 2017
+
+// Course: CSCI 2003 42733  Date Due: 31 January 2017
+
+// Instructor: Ms. Greer
+//
+// File name: EmployeeTester
+//
+// Program Description: A Employee tester class that checks whether the Employee class runs or not
+
+
+public class EmployeeTester
+{
+   public static void main(String[] args)
+   {
+   
+      // it creates two objects, object 1 has argument and object 2 has no argument
+      Employee object1 = new Employee("Tyler Greer",12345,"Computer Science","Instructor");   //with argument
+      Employee object2 = new Employee(); // without argument                                                    
+ 
+      // testing argument constructor
+      System.out.println("TESTING ARGUMENT CONSTRUCTOR");
+      System.out.println("----------------------------\n");       
+
+      System.out.println(object1.getName());
+      System.out.println("Expected: Tyler Greer\n");              
+      
+      System.out.println(object1.getIDn());
+      System.out.println("Expected: 12345\n");                    // printing the user's desired layout
+      
+      System.out.println(object1.getDepartment());
+      System.out.println("Expected: Computer Science\n");         
+
+      System.out.println(object1.getPosition());                 
+      System.out.println("Expected: Instructor\n");
+      
+      // testing no-argument constructor
+      System.out.println("TESTING NO-ARGUMENT CONSTRUCTOR");
+      System.out.println("-------------------------------\n");
+
+      // refering to setters 
+      object2.setName("Desiree Wilson");
+      object2.setIDn(45678);                                          // printing the user's desired layout
+      object2.setDepartment("Orthopaedics");
+      object2.setPosition("Registered Nurse");
+
+      // testing no-argument constructor again
+
+      System.out.println(object2.getName());
+      System.out.println("Expected: Desiree Wilson\n");
+   
+      System.out.println(object2.getIDn());
+      System.out.println("Expected: 45678\n");                       // printing the user's desired layout
+                                                   
+      System.out.println(object2.getDepartment());          
+      System.out.println("Expected: Orthopaedics\n");
+   
+      System.out.println(object2.getPosition());
+      System.out.println("Expected: Registered Nurse");
+
+
+   }
+}
